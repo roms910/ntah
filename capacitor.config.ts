a0ec500ff6d92a87f1902ae97540e8200ae7e1df
roms910 <ahmadhilmi0609@gmail.com>
2026-05-10 13:41:14 +0700

    fix: package id com.romzz.music

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,20 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.romzz.mono',
-  appName: 'monoromzz',
-  webDir: 'dist'
+  appId: 'com.romzz.music',
+  appName: 'Monochrome',
+  webDir: 'dist',
+  server: {
+    url: 'https://mono.romzz.biz.id',
+    cleartext: false
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: 'WEB_CLIENT_ID_KAMU.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
+  }
 };
 
 export default config;
